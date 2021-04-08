9adb3bb9a347a1f974a7ee785bca61dde9c59d50
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-08 11:56:58 -0300

    Centraliza imagem no card mantendo sua proporção

--- resources/views/layout/_includes/topo.blade.php
+++ resources/views/layout/_includes/topo.blade.php
@@ -8,6 +8,9 @@
   <!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
 
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
